d1af3e6d378cb9ca44e0f6bfeadaa4103982479a
sagar-cenation <sagarkeshri@rocketmail.com>
2016-08-21 12:36:51 +0530

    completed main page with responsiveness

--- sagar/main.php
+++ sagar/main.php
@@ -3,8 +3,17 @@
 <title>Main Page</title>
   <link rel="stylesheet" type="text/css" href="../materialize/css/materialize.min.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 
   <link rel="stylesheet" type="text/css" href="../materialize/css/main.css">
+  <script>
+  	$(document).ready(function(){
+    $(".button-collapse").sideNav();        
+	});
+
+
+
+  </script>
 </head>
 
 <body>
@@ -15,8 +24,8 @@
 				<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
 				<ul class="right hide-on-med-and-down">
 					<li><a href="main.php">Home</a></li>
-					<li><a href="#">Sign Up</a></li>
-					<li><a href="#">Sign In</a></li>
+					<li><a href="../frontend/signup_form.php">Sign Up</a></li>
+					<li><a href="../frontend/login_form.php">Sign In</a></li>
 					<li><a href="#"><i class="material-icons" id="search" onclick="activate()">search</i></a></li>
 					<form class="right" id="search_with_details" style="display:none;">
 				        <div class="input-field">
@@ -27,11 +36,17 @@
 				    </form>
 				</ul>
 				<ul class="side-nav" id="mobile-demo">
+					<li  style="height:100px;"><a href="main.php"><img src="#" alt="TPB icon"></a></li>
+					<li></li>
 					<li><a href="main.php">Home</a></li>
-					<li><a href="#">Sign Up</a></li>
-					<li><a href="#">Sign In</a></li>
-					<li><a href="#"><i class="material-icons" id="search">search</i></a></li>
-					
+					<li><a href="../frontend/signup_form.php">Sign Up</a></li>
+					<li><a href="../frontend/login_form.php">Sign In</a></li>
+			        <!--<li class="search ">
+			        	<div class="search-wrapper card">
+				            <input id="search"><i class="material-icons">search</i>
+				            <div class="search-results" style="display:none"></div>
+			        	</div>
+			        </li>-->				  
 				</ul>
 		    </div>
 	  	</nav>
@@ -43,48 +58,76 @@
 				<img src="../images/sample-1.jpg">
 				<span class="card-title">Card Title</span>
 			</div>
-			<div class="card-content">
+			<div class="card-content" style="max-height: 85px;">
 		    <p>
-				I am a very simple card. I am good at containing small bits of information.
+				I am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of information.
 				I am convenient because I require little markup to use effectively.
 		 	</p>
 	  		</div>
-			<div class="card-action" style="padding:10px 20px;">
-				<a href="#">Read More</a>
+		 	<a href="#" class="waves-effect waves-light btn " style="margin:15px">Read More</a>
+
+			<div class="card-action" style="padding:10px 20px; height:50px;">
+				<div class="left">
+				<div class="left"><img src="#" alt="img"></div>
+				<div class="right" style="margin-left:10px">
+					<div ><a href="#" style="font-size:0.75em; color: #757575">Sagar Keshri</a></div>
+					<div ><a href="#" style="font-size:0.75em; color: #757575">Date</a></div>
+
+				</div>
+				</div>
 			</div>
 		</div>
-
-		<div class="card hoverable medium article">
+		
+		<div class="card hoverable large article">
 			<div class="card-image">
 				<img src="../images/sample-1.jpg">
 				<span class="card-title">Card Title</span>
 			</div>
-			<div class="card-content">
+			<div class="card-content" style="max-height: 85px;">
 		    <p>
-				I am a very simple card. I am good at containing small bits of information.
+				I am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of information.
 				I am convenient because I require little markup to use effectively.
 		 	</p>
 	  		</div>
-			<div class="card-action" style="padding:10px 20px;">
-				<a href="#">Read More</a>
+		 	<a href="#" class="waves-effect waves-light btn " style="margin:15px">Read More</a>
+
+			<div class="card-action" style="padding:10px 20px; height:50px;">
+				<div class="left">
+				<div class="left"><img src="#" alt="img"></div>
+				<div class="right" style="margin-left:10px">
+					<div ><a href="#" style="font-size:0.75em; color: #757575">Sagar Keshri</a></div>
+					<div ><a href="#" style="font-size:0.75em; color: #757575">Date</a></div>
+
+				</div>
+				</div>
 			</div>
 		</div>
-
-		<div class="card hoverable medium article">
+		<div class="card hoverable large article">
 			<div class="card-image">
 				<img src="../images/sample-1.jpg">
 				<span class="card-title">Card Title</span>
 			</div>
-			<div class="card-content">
+			<div class="card-content" style="max-height: 85px;">
 		    <p>
-				I am a very simple card. I am good at containing small bits of information.
+				I am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of informI am a very simple card. I am good at containing small bits of information.
 				I am convenient because I require little markup to use effectively.
 		 	</p>
 	  		</div>
-			<div class="card-action" style="padding:10px 20px;">
-				<a href="#">Read More</a>
+		 	<a href="#" class="waves-effect waves-light btn " style="margin:15px">Read More</a>
+
+			<div class="card-action" style="padding:10px 20px; height:50px;">
+				<div class="left">
+				<div class="left"><img src="#" alt="img"></div>
+				<div class="right" style="margin-left:10px">
+					<div ><a href="#" style="font-size:0.75em; color: #757575">Sagar Keshri</a></div>
+					<div ><a href="#" style="font-size:0.75em; color: #757575">Date</a></div>
+
+				</div>
+				</div>
 			</div>
 		</div>
+
+		
 	</div>
 </body>
 
@@ -95,10 +138,7 @@
 		document.getElementById('search').style.display="none";
 		document.getElementById('search_with_details').style.display="block";
 	}
-	$(document).ready(function(){
-    $(".button-collapse").sideNav();        
-	});
-
+	
 </script>
 
 </html>
